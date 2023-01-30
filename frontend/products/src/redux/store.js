6bb7e42be8944b62eed60294f73dd30bbc61decc
manesha07 <manishagora52@gmail.com>
2023-01-30 16:23:17 +0545

    products folder deleted

--- frontend/products/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from "./cartSlice.js"
-
-export const store = configureStore({
-  reducer: {
-    /** @TODO Add your reducers here */
-    cart: cartReducer
-  },
-});
