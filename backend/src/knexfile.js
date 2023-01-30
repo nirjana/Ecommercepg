eb84858ae6fdbe832e568725884b8b6e8d3712b5
manesha07 <manishagora52@gmail.com>
2023-01-30 09:12:26 +0545

    changes in configuration of databse

--- backend/src/knexfile.js
+++ backend/src/knexfile.js
@@ -1,11 +1,31 @@
+// const configuration = {
+//   client: 'pg',
+//   connection: {
+//     host: 'localhost',
+//     port: 5432,
+//     user: 'postgres',
+//     password: 'postgres',
+//     database: 'ecdb'
+//   },
+//   migrations: {
+//     tableName: 'migrations',
+//   },
+//   seeds: {
+//     directory: './seeds',
+//     stub: './stub/seed.stub'
+//   }
+// };
+
+// export default configuration;
+
 const configuration = {
   client: 'pg',
   connection: {
-    host: 'localhost',
+    host: 'dpg-cfbckh82i3mjdult2ghg-a',
     port: 5432,
-    user: 'postgres',
-    password: 'postgres',
-    database: 'ecdb'
+    user: 'postgres_i9r4_user',
+    password: 'SGN2uA2ByFq5bPzs4xqGmbUi0CX47anO',
+    database: 'postgres_i9r4'
   },
   migrations: {
     tableName: 'migrations',
