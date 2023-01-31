a2be5f2dc080dec76db988ba39116d58169399d5
Sujata Gaihre <40952778+nirjana@users.noreply.github.com>
2023-01-31 10:50:14 +0545

    Update knexfile.js

--- backend/src/knexfile.js
+++ backend/src/knexfile.js
@@ -1,57 +1,11 @@
-const configuration = {
-  client: 'pg',
-  connection: {
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    password: 'postgres',
-    database: 'ecdb'
-  },
-  migrations: {
-    tableName: 'migrations',
-  },
-  seeds: {
-    directory: './seeds',
-    stub: './stub/seed.stub'
-  }
-};
-export default configuration;
-
-// const configuration = {
-//   client: "pg",
-//   connection: {
-
-//     host: "dpg-cfbldj82i3mjdumu9e90-a.singapore-postgres.render.com",
-//     port: 5432,
-//     user: "ecdb_rvzo_user",
-//     password: "qs4IfDMKQkVFHfi2YLA08RKdrk7lAO47",
-//     database: "ecdb_rvzo",
-
-//   // host: 'localhost',
-//    // port: 5432,
-//    // user: 'postgres',
-//     //password: 'postgres',
-//    // database: 'ecdb'
-//   },
-//   migrations: {
-//     tableName: "migrations",
-//   },
-//   seeds: {
-//     directory: "./seeds",
-//     stub: "./stub/seed.stub",
-//   },
-// };
-
-// export default configuration;
-
 // const configuration = {
 //   client: 'pg',
 //   connection: {
-//     host: 'dpg-cfbl73pgp3jsh6b4rrvg-a.oregon-postgres.render.com',
+//     host: 'localhost',
 //     port: 5432,
-//     user: 'postgres_40kd_user',
-//     password: 'qFLxDKieomoww8ta8fjnbhvjwwJaxFCI',
-//     database: 'postgres_40kd'
+//     user: 'postgres',
+//     password: 'postgres',
+//     database: 'ecdb'
 //   },
 //   migrations: {
 //     tableName: 'migrations',
@@ -61,5 +15,68 @@ export default configuration;
 //     stub: './stub/seed.stub'
 //   }
 // };
-
 // export default configuration;
+
+// // const configuration = {
+// //   client: "pg",
+// //   connection: {
+
+// //     host: "dpg-cfbldj82i3mjdumu9e90-a.singapore-postgres.render.com",
+// //     port: 5432,
+// //     user: "ecdb_rvzo_user",
+// //     password: "qs4IfDMKQkVFHfi2YLA08RKdrk7lAO47",
+// //     database: "ecdb_rvzo",
+
+// //   // host: 'localhost',
+// //    // port: 5432,
+// //    // user: 'postgres',
+// //     //password: 'postgres',
+// //    // database: 'ecdb'
+// //   },
+// //   migrations: {
+// //     tableName: "migrations",
+// //   },
+// //   seeds: {
+// //     directory: "./seeds",
+// //     stub: "./stub/seed.stub",
+// //   },
+// // };
+
+// // export default configuration;
+
+// // const configuration = {
+// //   client: 'pg',
+// //   connection: {
+// //     host: 'dpg-cfbl73pgp3jsh6b4rrvg-a.oregon-postgres.render.com',
+// //     port: 5432,
+// //     user: 'postgres_40kd_user',
+// //     password: 'qFLxDKieomoww8ta8fjnbhvjwwJaxFCI',
+// //     database: 'postgres_40kd'
+// //   },
+// //   migrations: {
+// //     tableName: 'migrations',
+// //   },
+// //   seeds: {
+// //     directory: './seeds',
+// //     stub: './stub/seed.stub'
+// //   }
+// // };
+
+// // export default configuration;
+
+
+
+
+
+const configuration = {
+    client: 'pg',
+  connection: {
+    host: "satao.db.elephantsql.com",
+    port: 5432,
+    user: "fajdksne",
+    password: "qm8Es4DxzQzhfiiGVnNq3deYnzH4Q-rR",
+    database: "fajdksne",
+  },
+    migrations: {
+      tableName: 'migrations',
+    },
