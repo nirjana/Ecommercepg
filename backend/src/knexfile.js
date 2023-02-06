794d2fd53082dd4494cbe1cd958256b8609215e0
manesha07 <manishagora52@gmail.com>
2023-02-06 23:41:16 +0545

    authentication and validation final

--- backend/src/knexfile.js
+++ backend/src/knexfile.js
@@ -1,45 +1,45 @@
-
-// const configuration = {
-//   client: 'pg',
-//   connection: {
-//     host: 'localhost',
-//     port: 5432,
-//     user: 'postgres',
-//     password: 'postgres',
-//     database: 'ecdb'
-//   },
-//   migrations: {
-//     tableName: 'migrations',
-//   },
-//   seeds: {
-//     directory: './seeds',
-//     stub: './stub/seed.stub'
-//   }
-// };
+ import Knex from 'knex';
+const configuration = {
+  client: 'pg',
+  connection: {
+    host: 'localhost',
+    port: 5432,
+    user: 'postgres',
+    password: 'postgres',
+    database: 'ecdb'
+  },
+  migrations: {
+    tableName: 'migrations',
+  },
+  seeds: {
+    directory: './seeds',
+    stub: './stub/seed.stub'
+  }
+};
 // export default configuration;
 
 //ELEPHANT SUJATA 
 //ELEPHANT
 
-import Knex from 'knex';
+// import Knex from 'knex';
 
-const configuration = {
-  client: "pg",
-  connection: {
-    host: "satao.db.elephantsql.com",
-    port: 5432,
-    user: "ixfkndjf",
-    password: "pai8KiXmaa-ObCf32QNLfPddm1MpoOJ0",
-    database: "ixfkndjf",
-  },
-  migrations: {
-    tableName: "migrations",
-  },
-  seeds: {
-    directory: "./seeds",
-    stub: "./stub/seed.stub",
-  },
-};
+// const configuration = {
+//   client: "pg",
+//   connection: {
+//     host: "satao.db.elephantsql.com",
+//     port: 5432,
+//     user: "ixfkndjf",
+//     password: "pai8KiXmaa-ObCf32QNLfPddm1MpoOJ0",
+//     database: "ixfkndjf",
+//   },
+//   migrations: {
+//     tableName: "migrations",
+//   },
+//   seeds: {
+//     directory: "./seeds",
+//     stub: "./stub/seed.stub",
+//   },
+// };
 export const connection = Knex(configuration);
 export default configuration;
 
