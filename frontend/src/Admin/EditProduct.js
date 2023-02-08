fdf38902ebdc2877e96d5afe5812ee38cec99260
Sujata Gaihre <sujatag391@gmail.com>
2023-02-08 23:21:53 +0545

    image_uploader_complete(frontend and backend)

--- frontend/src/Admin/EditProduct.js
+++ frontend/src/Admin/EditProduct.js
@@ -3,103 +3,156 @@ import { useParams } from 'react-router-dom';
 import { ToastContainer} from 'react-toastify';
 import * as notify from "../utils/notify.js";
 import authHeader from '../authentication/authHeader.js';
+import axios from "axios";
+
+
 
 export default function EditProduct() {
   const [title,setTitle] = useState("");
   const [description,setDescription] = useState("");
-  const [image,setImage] = useState("");
+    const [image, setImage] = useState(null);
   const [price,setPrice] =useState("");
   const [category,setCategory] =useState("");
   const [stock,setStock] = useState("");
   const {id} =useParams();
 
+
+   const handleImageChange = (event) => {
+     setImage(event.target.files[0]);
+     console.log(image, "im imahe");
+   };
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    // fetch(`http://localhost:8000/products/${id}`, {
-      fetch(`${process.env.REACT_APP_API_URL}/products/${id}`, {
-      method: 'PUT', 
-      headers: authHeader(),
-      body: JSON.stringify({name:title,description:description,price:price,stock:stock,category:category,images:image}),
-    })
-    .then((response) => response.json())
-    .then((data) => {console.log(data.details)
-      if(!data.details)
-     { console.log('Success:', data);
-      notify.success("Edited")}
-      else{
-        notify.error(data.details)
-      }
-    })
-      .catch((error) => {
-        notify.error(error)
-        console.error('Error:', error);
-      });
+
+    //  const adminToken = JSON.parse(localStorage.getItem("token"));
+    const formData = new FormData();
+    console.log("form", formData);
+    formData.append("name", title);
+    formData.append("description", description);
+    formData.append("price", price);
+    formData.append("stock", stock);
+    formData.append("category", category);
+    formData.append("image", image);
+    console.log(image);
+
+   axios
+     .post(`${process.env.REACT_APP_API_URL}/products`, formData, {
+       method: "POST", // or 'PUT'
+
+       headers: {
+         "Content-Type": "multipart/form-data",
+       },
+     })
+     .then((response) => response.data)
+     .then((data) => {
+       console.log(data.details);
+       if (!data.details) {
+         console.log("Success:", data);
+         notify.success("Edited");
+       } else {
+         notify.error(data.details);
+       }
+     })
+     .catch((error) => {
+       notify.error(error);
+       console.error("Error:", error);
+     });
   }
 
   return (
     <>
-    <form onSubmit={handleSubmit} className="shadow-xl mx-auto w-[300px]  p-[30px] mt-[40px] rounded-md">
-      <h1 className="text-[30px] text-center mb-[20px]">Edit Product</h1>
+      <form
+        onSubmit={handleSubmit}
+        className="shadow-xl mx-auto w-[300px]  p-[30px] mt-[40px] rounded-md"
+      >
+        <h1 className="text-[30px] text-center mb-[20px]">Edit Product</h1>
 
-      <label for ="title" className='text-gray-600'> Product Name</label>
-      <input 
-        type="text" 
-        name="title" 
-        placeholder={"Product Name"}
-        value={title || ""} 
-        onChange={(e)=>{setTitle(e.target.value)}}
-      />
-      <label for ="description" className='text-gray-600'> Product Description</label>
-        <input 
-          type="text" 
-          name="description" 
+        <label for="title" className="text-gray-600">
+          {" "}
+          Product Name
+        </label>
+        <input
+          type="text"
+          name="title"
+          placeholder={"Product Name"}
+          value={title || ""}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+        <label for="description" className="text-gray-600">
+          {" "}
+          Product Description
+        </label>
+        <input
+          type="text"
+          name="description"
           placeholder={"Description"}
-          value={description || ""} 
-          onChange={(e)=>{setDescription(e.target.value)}}
+          value={description || ""}
+          onChange={(e) => {
+            setDescription(e.target.value);
+          }}
         />
         <div>
-      <label for ="image" className='text-gray-600'> Product Image</label>
-        <input 
-          type="text" 
-          name="image" 
-          placeholder={"image"}
-          value={image || ""} 
-          onChange={(e)=>{setImage(e.target.value)}}
-        />
+          <label for="image" className="text-gray-600">
+            Product Image
+          </label>
+          <input type="file" name="image" onChange={handleImageChange} />
         </div>
         <div>
-        <label for ="price" className='text-gray-600'> Product Price</label>
-        <input 
-          type="number" 
-          name="price" 
-          placeholder={"Price"}
-          value={price || ""} 
-          onChange={(e)=>{setPrice(e.target.value)}}
-        />
+          <label for="price" className="text-gray-600">
+            {" "}
+            Product Price
+          </label>
+          <input
+            type="number"
+            name="price"
+            placeholder={"Price"}
+            value={price || ""}
+            onChange={(e) => {
+              setPrice(e.target.value);
+            }}
+          />
         </div>
         <div>
-        <label for ="category" className='text-gray-600'> Product Category</label>
-        <input 
-          type="text" 
-          name="category" 
-          placeholder={"Category"}
-          value={category|| ""} 
-          onChange={(e)=>{setCategory(e.target.value)}}
-        />
+          <label for="category" className="text-gray-600">
+            {" "}
+            Product Category
+          </label>
+          <input
+            type="text"
+            name="category"
+            placeholder={"Category"}
+            value={category || ""}
+            onChange={(e) => {
+              setCategory(e.target.value);
+            }}
+          />
         </div>
         <div>
-        <label for ="stock" className='text-gray-600'> Product Stock</label>
-          <input 
-          type="number" 
-          name="stock" 
-          placeholder={"Stock"}
-          value={stock || ""} 
-          onChange={(e)=>{setStock(e.target.value)}}
-        />
+          <label for="stock" className="text-gray-600">
+            {" "}
+            Product Stock
+          </label>
+          <input
+            type="number"
+            name="stock"
+            placeholder={"Stock"}
+            value={stock || ""}
+            onChange={(e) => {
+              setStock(e.target.value);
+            }}
+          />
         </div>
-        <button type="submit" className="shadow-md p-[5px] w-full mb-[20px] mt-[10px] text-white rounded-md bg-green-600" >Submit</button>
-    </form>
-        <ToastContainer autoClose={4000}/>
-        </>
-  )
+        <button
+          type="submit"
+          className="shadow-md p-[5px] w-full mb-[20px] mt-[10px] text-white rounded-md bg-green-600"
+        >
+          Submit
+        </button>
+      </form>
+      <ToastContainer autoClose={4000} />
+    </>
+  );
 }
