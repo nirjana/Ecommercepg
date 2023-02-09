4401638b2344181325cbcb2a322254abf46086c3
Sujata Gaihre <sujatag391@gmail.com>
2023-02-09 14:17:44 +0545

    Loging Test Cases

--- frontend/src/Component/Login.test.js
+++ frontend/src/Component/Login.test.js
@@ -1,30 +1,76 @@
-import { render } from "@testing-library/react";
-import "@testing-library/jest-dom";
-import { fireEvent, waitFor, screen } from "@testing-library/react";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { store } from "../redux/store";
+import React from "react";
+import { render, fireEvent, waitFor, screen } from "@testing-library/react";
+import { MemoryRouter as Router } from "react-router-dom";
 import Login from "./Login";
-import userEvent from "@testing-library/user-event";
+import * as notify from "../utils/notify.js";
+import { ModuleMocker } from "jest-mock";
 
-test("allows the user to User login successfully", async () => {
-  render(
-    <Provider store={store}>
-      <BrowserRouter>
+jest.mock("../utils/notify", () => ({
+  success: jest.fn(),
+  error: jest.fn(),
+}));
+
+jest.mock("react-toastify", () => ({
+  ToastContainer: () => <div />,
+}));
+
+describe("Login", () => {
+  beforeEach(() => {
+    localStorage.clear();
+  });
+
+
+  it("should render login form and submit successfully", async () => {
+    const mockSuccessResponse = {
+      data: {
+        token: "token",
+        user: {
+          name: "user",
+        },
+      },
+    };
+
+    render(
+      <Router>
         <Login />
-      </BrowserRouter>
-    </Provider>
-  );
+      </Router>
+    );
 
-  const usernameInput = screen.getByTestId("login-user");
-  userEvent.type(usernameInput, "admin");
-  expect(screen.getByTestId("login-user")).toHaveValue("admin");
+    const usernameInput = screen.getByTestId("login-user");
+    const passwordInput = screen.getByTestId("password");
+    const submitButton = screen.getByTestId("submit");
 
-  const passwordInput = screen.getByTestId("password");
+    fireEvent.change(usernameInput, { target: { value: "user" } });
+    fireEvent.change(passwordInput, { target: { value: "ram_admin_2" } });
+    fireEvent.click(submitButton);
 
-  userEvent.type(passwordInput, "admin1");
-  expect(screen.getByTestId("password")).toHaveValue("admin1");
-  fireEvent.click(screen.getByTestId("submit"));
+    const mockResponse = {
+      status: 201,
+      json: () => Promise.resolve(mockSuccessResponse),
+    };
+    console.log(mockResponse, "im response");
 
-  expect(window.location.pathname).toBe("/");
+    const fetch = jest.spyOn(global, "fetch");  //Jest's spyOn function to mock the fetch API and intercept any calls made to it.
+    // fetch.mockImplementation((url, options) => {
+    //   if (options.method === 'POST') {
+    //     return Promise.resolve( mockResponse);
+    //   }
+    // });
+    fetch.mockImplementation(() => Promise.resolve(mockResponse));
+
+    console.log(fetch(`${process.env.REACT_APP_API_URL}/login`),"im fatch")
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name: "Test resource" }),
+    });
+    expect(response.status).toBe(201);
+    const json = await response.json();
+
+    console.log("json", json.data);
+    expect(json).toEqual({ data: { token: "token", user: { name: "user" } } });
+  });
+
 });
